6797a2dfee5525b867e84a33f78a18637eaf789b
guy-dar <guyd1995@gmail.com>
2025-09-25 18:20:06 +0300

    make generation script more convenient

--- generate_embeddings.py
+++ generate_embeddings.py
@@ -18,35 +18,34 @@ def get_embeddings(model, ds, batch_size=32):
     return embeddings_tensor
 
 if __name__ == "__main__":
+    encoder_dict = {
+        'e5': 'intfloat/e5-base-v2',
+        'stella': 'infgrad/stella-base-en-v2',
+        'granite': 'ibm-granite/granite-embedding-278m-multilingual',
+        'gtr': 'sentence-transformers/gtr-t5-base',
+        'granite-small': 'ibm-granite/granite-embedding-30m-english',
+        'e5-small': 'intfloat/e5-small',
+        'all-mini': 'all-MiniLM-L6-v2'
+    }
+
     parser = argparse.ArgumentParser(description="Generate and save sentence embeddings.")
     parser.add_argument("--output_dir", type=str,
                         help="Directory to save the generated embeddings.")
     parser.add_argument("--batch_size", type=int, default=32,
                         help="Batch size for generating embeddings.")
+    parser.add_argument('--encoders', type=str, default='e5,stella,granite,gtr',
+                        help="Comma-separated list of model names to use for embeddings.")
     args = parser.parse_args()
 
     output_dir = args.output_dir
     batch_size = args.batch_size
+    encoder_names = args.encoders.split(',')
+    assert all(enc in encoder_dict for enc in encoder_names), "One or more specified encoders are not recognized."
 
     ds = load_dataset("jxm/nq_corpus_dpr", split="train").shuffle(seed=42).select(range(60_000))
-    sent_models = {}
-    sent_embeds = {}
-
-    sent_keys = {
-        'e5': 'intfloat/e5-base-v2',
-        'stella': 'infgrad/stella-base-en-v2',
-        'granite': 'ibm-granite/granite-embedding-278m-multilingual',
-        'gtr': 'sentence-transformers/gtr-t5-base'
-        # 'granite-small': 'ibm-granite/granite-embedding-30m-english',
-        # 'e5-small': 'intfloat/e5-small',
-        # 'all-mini': 'all-MiniLM-L6-v2'
-    }
-    for key in sent_keys:
-        if key not in sent_models:
-            sent_models[key] = SentenceTransformer(sent_keys[key])
-            sent_embeds[key] = get_embeddings(sent_models[key], ds, batch_size=batch_size)
 
     os.makedirs(output_dir, exist_ok=True)
-
-    for key, tensor in sent_embeds.items():
-        torch.save(tensor, f"{output_dir}/{key}.pt")
+    for key in encoder_names:
+        encoder = SentenceTransformer(encoder_dict[key]).to(device)
+        sent_embeds = get_embeddings(encoder, ds, batch_size=batch_size)
+        torch.save(sent_embeds, f"{output_dir}/{key}.pt")
